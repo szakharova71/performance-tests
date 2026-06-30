55676d7f7f7ea842a4db18b79fc63c4d1a3d962a
szakharova71 <szakharova86@gmail.com>
2026-06-30 12:08:15 +0300

    Знакомство с HTTP API клиентами

---
+++ httpx_client.py
@@ -0,0 +1,24 @@
+import time
+
+import httpx
+
+# Инициализируем клиент с base_url
+client = httpx.Client(
+    base_url="http://localhost:8003",
+    timeout=100, # Таймаут в секундах
+    headers={"Authorization": "Bearer ..."}
+)
+
+payload = {
+    "email": f"user.{time.time()}@example.com",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string",
+    "phoneNumber": "string"
+}
+
+# Выполняем POST-запрос, используя относительный путь
+response = client.post("/api/v1/users", json=payload)
+
+# Выводим ответ в консоль
+print(response.text)
